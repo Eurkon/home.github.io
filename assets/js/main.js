77e9f8a8d7e55156d8f740ae02b2952708d98c47
Eurkon <1124724692@qq.com>
2023-07-04 18:25:15 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,104 +1,86 @@
 var iUp = (function () {
-	var time = 0,
-		duration = 150,
-		clean = function () {
-			time = 0;
-		},
-		up = function (element) {
-			setTimeout(function () {
-				element.classList.add("up");
-			}, time);
-			time += duration;
-		},
-		down = function (element) {
-			element.classList.remove("up");
-		},
-		toggle = function (element) {
-			setTimeout(function () {
-				element.classList.toggle("up");
-			}, time);
-			time += duration;
-		};
-	return {
-		clean: clean,
-		up: up,
-		down: down,
-		toggle: toggle
-	};
-})();
+  var time = 0,
+    duration = 150,
+    clean = function () {
+      time = 0;
+    },
+    up = function (element) {
+      setTimeout(function () {
+        element.classList.add("up");
+      }, time);
+      time += duration;
+    },
+    down = function (element) {
+      element.classList.remove("up");
+    },
+    toggle = function (element) {
+      setTimeout(function () {
+        element.classList.toggle("up");
+      }, time);
+      time += duration;
+    };
+  return {
+    clean: clean,
+    up: up,
+    down: down,
+    toggle: toggle
+  };
+})()
 
-function getBingImages(imgUrls) {
-	/**
-	 * 获取Bing壁纸
-	 * 先使用 GitHub Action 每天获取 Bing 壁纸 URL 并更新 images.json 文件
-	 * 然后读取 images.json 文件中的数据
-	 */
-	var indexName = "bing-image-index";
-	var index = sessionStorage.getItem(indexName);
-	var panel = document.querySelector('#panel');
-	if (isNaN(index) || index == 7) index = 0;
-	else index++;
-	var imgUrl = imgUrls[index];
-	var url = "https://www.bing.com" + imgUrl;
-	panel.style.background = "url('" + url + "') center center no-repeat #666";
-	panel.style.backgroundSize = "cover";
-	sessionStorage.setItem(indexName, index);
+function getBingImages (imgUrls) {
+  /**
+   * 获取Bing壁纸
+   * 先使用 GitHub Action 每天获取 Bing 壁纸 URL 并更新 images.json 文件
+   * 然后读取 images.json 文件中的数据
+   */
+  var indexName = 'bing-image-index'
+  var index = sessionStorage.getItem(indexName)
+  var $panel = document.getElementById('background')
+  if (isNaN(index) || index == 7) index = 0
+  else index++
+  var imgUrl = imgUrls[index]
+  var url = 'https://www.bing.com' + imgUrl
+  $panel.style.background = `url("${url}") center center no-repeat #666`
+  $panel.style.backgroundSize = 'cover'
+  sessionStorage.setItem(indexName, index)
 }
 
-function decryptEmail(encoded) {
-	var address = atob(encoded);
-	window.location.href = "mailto:" + address;
+function decryptEmail (encoded) {
+  var address = atob(encoded)
+  window.location.href = 'mailto:' + address
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-	// 获取一言数据
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {
-			var res = JSON.parse(this.responseText);
-			document.getElementById('description').innerHTML = res.hitokoto + "<br/> -「<strong>" + res.from + "</strong>」";
-		}
-	};
-	xhr.open("GET", "https://v1.hitokoto.cn", true);
-	xhr.send();
+function getHitokoto () {
+  fetch('https://v1.hitokoto.cn')
+    .then(function (res) {
+      return res.json()
+    })
+    .then(function (res) {
+      document.getElementById('description').innerHTML = res.hitokoto + '<br/> -「<strong>' + res.from + '</strong>」'
+    })
+}
 
-	var iUpElements = document.querySelectorAll(".iUp");
-	iUpElements.forEach(function (element) {
-		iUp.up(element);
-	});
+function resizeFn () {
+  if (document.querySelector('#panel').clientHeight < document.querySelector('.panel-main').clientHeight) {
+    document.querySelector('#panel').style.overflow = 'auto'
+    document.querySelector('#panel').style.alignItems = 'Initial'
+  } else {
+    document.querySelector('#panel').style.overflow = ''
+    document.querySelector('#panel').style.alignItems = ''
+  }
+}
 
-	var avatarElement = document.querySelector(".js-avatar");
-	avatarElement.addEventListener('load', function () {
-		avatarElement.classList.add("show");
-	});
-});
+window.addEventListener('load', event => {
+  resizeFn()
+  window.addEventListener('resize', resizeFn)
+  getHitokoto()
 
-var btnMobileMenu = document.querySelector('.btn-mobile-menu__icon');
-var navigationWrapper = document.querySelector('.navigation-wrapper');
+  document.querySelectorAll('.iUp').forEach(function (e) {
+    iUp.up(e)
+  })
 
-btnMobileMenu.addEventListener('click', function () {
-	if (navigationWrapper.style.display == "block") {
-		navigationWrapper.addEventListener('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
-			navigationWrapper.classList.toggle('visible');
-			navigationWrapper.classList.toggle('animated');
-			navigationWrapper.classList.toggle('bounceOutUp');
-			navigationWrapper.removeEventListener('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', arguments.callee);
-		});
-		navigationWrapper.classList.toggle('animated');
-		navigationWrapper.classList.toggle('bounceInDown');
-		navigationWrapper.classList.toggle('animated');
-		navigationWrapper.classList.toggle('bounceOutUp');
-	} else {
-		navigationWrapper.classList.toggle('visible');
-		navigationWrapper.classList.toggle('animated');
-		navigationWrapper.classList.toggle('bounceInDown');
-	}
-	btnMobileMenu.classList.toggle('social');
-	btnMobileMenu.classList.toggle('iconfont');
-	btnMobileMenu.classList.toggle('icon-list');
-	btnMobileMenu.classList.toggle('social');
-	btnMobileMenu.classList.toggle('iconfont');
-	btnMobileMenu.classList.toggle('icon-angleup');
-	btnMobileMenu.classList.toggle('animated');
-	btnMobileMenu.classList.toggle('fadeIn');
-});
+  const $avatar = document.querySelectorAll('.js-avatar')[0]
+  $avatar.onload = function () {
+    $avatar.classList.add('show')
+  }
+})
